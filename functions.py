4dca9da0ca66f3c67c38035112d48c0953fbc87e
Samuel Ogunjimi <42714945+spw3bt3ch@users.noreply.github.com>
2024-10-24 16:18:54 +0100

    Update functions.py
    refresh

--- functions.py
+++ functions.py
@@ -4,22 +4,29 @@ import requests
 
 
 def ai(bmi_result, username):
-	url = "https://gemini-pro-ai.p.rapidapi.com/"
-	
-	payload = { "contents": [
-			{
-				"role": "user",
-				"parts": [{ "text": f"An extremely short medical advice for {username} with a bmi value of {bmi_result}, address the name in your response" }]
-			}
-		] }
-	headers = {
-		"x-rapidapi-key": "4d7f06c497msh8ae3ad75c4ed8a9p1c158fjsn39f68bdb2dc7",
-		"x-rapidapi-host": "gemini-pro-ai.p.rapidapi.com",
-		"Content-Type": "application/json"
-	}
-	
-	response = requests.post(url, json=payload, headers=headers)
+    url = "https://chatgpt-42.p.rapidapi.com/conversationgpt4-2"
 
-    	resp = dict(response.json())
-    	for k, v in resp.items():
-        	st.info(f"{k.upper()}: {v}")
+    payload = {
+        "messages": [
+            {
+                "role": "user",
+                "content": f"An extremely short medical advice for {username} with a bmi value of {bmi_result}, address the name in your response"
+            }
+        ],
+        "system_prompt": "",
+        "temperature": 0.9,
+        "top_k": 5,
+        "top_p": 0.9,
+        "max_tokens": 256,
+        "web_access": False
+    }
+    headers = {
+        "x-rapidapi-key": "4d7f06c497msh8ae3ad75c4ed8a9p1c158fjsn39f68bdb2dc7",
+        "x-rapidapi-host": "chatgpt-42.p.rapidapi.com",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    resp = dict(response.json())
+    for k, v in resp.items():
+        st.info(f"{k.upper()}: {v}")
